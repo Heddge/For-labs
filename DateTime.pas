062e49507d2cbf218ff7550fb37626ee520639fb
Egor Goncharov <forskype1919@gmail.com>
2024-10-04 23:29:11 +0300

    Add convert from minutes to seconds

--- DateTime.pas
+++ DateTime.pas
@@ -4,5 +4,6 @@
   Assert(year > 0);
   if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
     flag := True;
-  Print($'Год високосный -> {flag}');
+  Println($'Год високосный -> {flag}');
+  Print('Количество секунд в минуте -> 60');
 end.
