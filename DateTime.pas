ffa58ead80906cff1f66432afd42e560571ce6e8
Egor Goncharov <forskype1919@gmail.com>
2024-10-04 23:30:36 +0300

    Revert "Add convert from minutes to seconds"
    This reverts commit 062e49507d2cbf218ff7550fb37626ee520639fb.

--- DateTime.pas
+++ DateTime.pas
@@ -4,6 +4,5 @@
   Assert(year > 0);
   if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
     flag := True;
-  Println($'Год високосный -> {flag}');
-  Print('Количество секунд в минуте -> 60');
+  Print($'Год високосный -> {flag}');
 end.
