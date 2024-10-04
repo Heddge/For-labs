1e1371571b9c2ec2d976908187618ec7c5fb9178
Egor Goncharov <forskype1919@gmail.com>
2024-10-04 23:55:29 +0300

    Add count days in Year

--- DateTime.pas
+++ DateTime.pas
@@ -8,7 +8,13 @@
   var (day1, month1, day2, month2) := ReadInteger4('Введите число и месяц первой даты, аналогично - второй:');
   Assert((day1 < 32) and (day2 < 32) and (month1 < 13) and (month2 < 13));
   if (30*(month1-1 + day1)) < (30*(month2 - 1) + day2) then
-    Print('2')
+    Println('2')
   else
-    Print('1');
+    Println('1');
+  var amount_days : Integer;
+  if flag then
+    amount_days := 366
+  else
+    amount_days := 365;
+  Println($'Количество дней в {year} году - > {amount_days}');
 end.
