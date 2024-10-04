3e203f3cd73bbd577954a3b3ebc1fad93ddd89a6
Egor Goncharov <forskype1919@gmail.com>
2024-10-04 23:25:27 +0300

    Add is leap year program

---
+++ DateTime.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var year := ReadInteger('Введите год:');
+  var flag := False;
+  Assert(year > 0);
+  if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
+    flag := True;
+  Print($'Год високосный -> {flag}');
+end.
